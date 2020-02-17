6f00f1b08564247e9868ebf3144b20605293a8fe
Douglas Henrique <doughrqe@gmail.com>
2020-02-17 18:03:37 -0300

    Inserindo barra de pesquisa

--- src/Pages/ListProducts/ListProducts.jsx
+++ src/Pages/ListProducts/ListProducts.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import './ListProducts.css'
+import ListFilter from './Components/ListFilter'
 class ListProducts extends React.Component {
   constructor(props) {
     super(props);
@@ -12,9 +13,15 @@ class ListProducts extends React.Component {
   render() {
     const { value, results } = this.state;
     return (
-      <div>
-        <input type="text" onChange={(e) => this.setState({ results: e.target.value })} />
-        <h1>{(results === '') ? value : results}</h1>
+      <div id="maxContain">
+        <div id="SearchList">
+          <ListFilter />
+        </div>
+        <div id="header">
+          <img src="https://image.flaticon.com/icons/svg/126/126083.svg" alt="carrinho de compras" />
+          <input id="searchBar" type="text" onChange={(e) => this.setState({ results: e.target.value })} />
+          <h1>{(results === '') ? value : results}</h1>
+        </div>
       </div>
     );
   }
