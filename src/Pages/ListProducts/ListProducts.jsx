faebb4a63b302423c4aad42ef1b5c67b051eb7e8
Douglas Henrique <doughrqe@gmail.com>
2020-02-17 15:41:41 -0300

    Criando pagina inicial vazia sem css

---
+++ src/Pages/ListProducts/ListProducts.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class ListProducts extends React.Component {
+  constructor(props){
+    super(props);
+    this.state= {
+      value: 'Você ainda não realizou uma Busca',
+      results: ''
+    }
+  }
+
+  render() {
+    const { value , results} = this.state;
+    return (
+      <div>
+        <input type="text" onChange={(e) => this.setState({results : e.target.value})}/>
+    <h1>{(results === '')? value: results}</h1>
+      </div>
+    );
+  }
+}
+
+export default ListProducts
