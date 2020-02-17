bf07751ea5c7f1296eda7ab33cfa4d660446d816
EddyeBoy27 <lipe0503@gmail.com>
2020-02-17 18:15:44 -0300

    Atualizando ShoppingCart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import ShoppingCart from './Pages/ShoppingCart/ShoppingCart';
 
 function App() {
   return (
-    <div />
+    <div>
+      <ShoppingCart />
+    </div>
   );
 }
 
---
+++ src/Pages/ShoppingCart/ShoppingCart.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import emptyBox from './images/empty_box.png';
+import emptyShopCar from './images/empty_car.svg';
+import '../ShoppingCart/ShoppingCart.css';
+import backButton from './images/backButton.png';
+
+export default class ShoppingCart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      load: false,
+      items: [],
+    };
+  }
+
+  loadingEmpty() {
+    return (
+      <div className="shoppingCart">
+        <div className="buttonReturn">
+          <img src={backButton} alt="backButton" />
+        </div>
+        <div className="clear" />
+        <div className="emptyCarPai">
+          <div className="emptyCar">
+            <img src={emptyShopCar} alt="emptyCar" />
+          <span>
+            Carrinho de Compras
+          </span>
+          </div>
+        </div>
+        <div className="emptyBoxPai">
+          <img className="emptyBox-Img" src={emptyBox} alt="emptyBox" />
+          Seu carrinho de compras está vazio.
+        </div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        {this.loadingEmpty()}
+      </div>
+    );
+  }
+}
