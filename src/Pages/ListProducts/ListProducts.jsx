a1ba93258c99102f7e4390bdea1a8a9e6ac133f1
EddyeBoy27 <lipe0503@gmail.com>
2020-02-18 04:45:30 -0300

    Estilizado SearchBar e Lupa

--- src/Pages/ListProducts/ListProducts.jsx
+++ src/Pages/ListProducts/ListProducts.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './ListProducts.css';
 import ListFilter from './Components/ListFilter';
 import CardProduct from './Components/CardProduct';
+import lupa from './images/lupa.png';
 
 class ListProducts extends React.Component {
   constructor(props) {
@@ -9,6 +10,7 @@ class ListProducts extends React.Component {
     this.state = {
       value: 'Você ainda não realizou uma Busca',
       results: [],
+      shopCart: [],
     };
     this.pesquisa = this.pesquisa.bind(this);
   }
@@ -37,11 +39,18 @@ class ListProducts extends React.Component {
         </div>
         <div className="header">
           <img src="https://image.flaticon.com/icons/svg/126/126083.svg" alt="carrinho de compras" />
-          <input
-            className="searchBar"
-            type="text"
-            onKeyDown={(e) => { if (e.key === 'Enter') this.pesquisa(e); }}
-          />
+          <div>
+            <div className="caixaLupa">
+              <input
+                className="searchBar"
+                type="text"
+                onKeyDown={(e) => { if (e.key === 'Enter') this.pesquisa(e); }}
+              />
+              <div className="lupa">
+                <img src={lupa} alt="lupa" />
+              </div>
+            </div>
+          </div>
           {(Object.keys(results).length === 0) ?
             <h1>{value}</h1> :
             <CardProduct arrCard={results} />
--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
+
+
+if (module.hot) {
+  module.hot.accept();
+  }
+
