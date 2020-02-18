d08ed7a3216e185779af3ddd125cc5c56fd0518a
Andrey <andreytsuzuki@gmail.com>
2020-02-18 16:14:02 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
 
-import ProductDetails from './Pages/ProductDetails/ProductDetails';
-
 function App() {
   return (
-    <ProductDetails />
+    <div></div>
   );
 }
 
