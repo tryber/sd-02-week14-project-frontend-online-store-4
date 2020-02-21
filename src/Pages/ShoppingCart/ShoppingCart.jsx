ab63cb234d9d12a7557105b5d836268ad1abdd7b
Douglas Henrique <doughrqe@gmail.com>
2020-02-21 00:53:22 -0300

    Shopping cart com valor total e adicionando e removendo produtos

--- src/Pages/ShoppingCart/ShoppingCart.jsx
+++ src/Pages/ShoppingCart/ShoppingCart.jsx
@@ -49,7 +49,8 @@ export default class ShoppingCart extends Component {
     this.carregaProdutos = this.carregaProdutos.bind(this);
     this.totalProdutos = this.totalProdutos.bind(this);
     this.finalizaCompra = this.finalizaCompra.bind(this);
-    this.atualizaContador = this.atualizaContador.bind(this);
+    this.abaixaContador = this.abaixaContador.bind(this);
+    this.aumentaContador = this.aumentaContador.bind(this);
   }
 
   componentDidMount() {
@@ -63,122 +64,139 @@ export default class ShoppingCart extends Component {
 
   atualizaState(infoKey, i) {
     const objKeys = JSON.parse(localStorage.getItem(infoKey[i]));
-    if (Object.keys(objKeys).length > 0) {
+    if (Object.keys(objKeys)) {
       this.setState((state) => ({
         load: true,
         items: [...state.items, objKeys],
       }));
-
-    }
-
-    quantidadeProduto(count) {
-      return (
-        <div className="somaReduz">
-          <p>{count}</p>
-        </div>
-      );
-    }
-
-    clearProduto() {
-      return (
-        <button
-          className="SomeAndRemove"
-          type="button"
-        >
-          <i className="material-icons">
-            clear
-        </i>
-        </button>
-      );
-    }
-
-    carregaImagemTitulo(title, image) {
-      return (
-        <div className="imagemTitulo">
-          <div>
-            <img src={image} alt={title}></img>
-          </div>
-          <div>
-            <p>{title}</p>
-          </div>
-        </div>
-      );
-    }
-
-    atualizaContador(value) {
-      const { items } = this.state
-      const contador = items.find((valor) => valor.id === value).count
-      this.quantidadeProduto(contador)
-    }
-
-    carregaProdutos(items, index) {
-      const { title, price, thumbnail, count, id } = items;
-      return (
-        <div className="containerCarrega" key={index}>
-          {this.clearProduto()}
-          {this.carregaImagemTitulo(title, thumbnail)}
-          <div className="containerBotoes">
-            <div className="somaReduz">
-              <button
-                className="SomeAndRemove"
-                type="button"
-                onClick={() => this.atualizaContador(id)}
-              >
-                <i className="material-icons">remove</i>
-              </button>
-            </div>
-            {this.quantidadeProduto(count)}
-            <div className="somaReduz">
-              <button
-                className="SomeAndRemove"
-                type="button"
-                onClick={() => this.setState((state) => ({ count: state.count + 1 }))}
-              >
-                <i className="material-icons">add</i>
-              </button>
-            </div>
-            {this.valorProduto(price)}
-          </div>
-        </div>
-      );
-    }
-
-    valorProduto(price) {
-      return (
-        <div className="valorProduto">
-          <p>R$ {price}</p>
-        </div>
-      );
-    }
-
-    totalProdutos() {
-      return (
-        <div>
-          Valor Total da Compra: R$ XXXX,XX
-      </div>
-      );
-    }
-
-    finalizaCompra() {
-      return (
-        <div>
-          <button>
-            Finalizar Compra
-        </button>
-        </div>
-      );
-    }
-
-    render() {
-      const { load, items } = this.state;
-      if (!load) return ShoppingCart.loadingEmpty();
-      return (
-        <div className="containerGeral">
-          {ShoppingCart.botaoVolta()}
-          {items.map((item, index) => this.carregaProdutos(item, index))}
-          {this.totalProdutos()}
-          {this.finalizaCompra()}
-        </div>
-      );
     }
   }
+
+  quantidadeProduto(count) {
+    return (
+      <div className="somaReduz">
+        <p>{count}</p>
+      </div>
+    );
+  }
+
+  clearProduto() {
+    return (
+      <button
+        className="SomeAndRemove"
+        type="button"
+      >
+        <i className="material-icons">
+          clear
+        </i>
+      </button>
+    );
+  }
+
+  carregaImagemTitulo(title, image) {
+    return (
+      <div className="imagemTitulo">
+        <div>
+          <img src={image} alt={title}></img>
+        </div>
+        <div>
+          <p>{title}</p>
+        </div>
+      </div>
+    );
+  }
+  aumentaContador(value) {
+    const { items } = this.state;
+    const index = items.indexOf(items.find(e => e.id === value));
+    items[index].count += 1;
+    this.setState({ items: items });
+    localStorage.setItem(value, JSON.stringify(items[index]));
+    localStorage.setItem('CartCount', Number(localStorage.getItem('CartCount')) + 1);
+  }
+  abaixaContador(value) {
+    const { items } = this.state;
+    const index = items.indexOf(items.find(e => e.id === value));
+    if (items.find(e => e.id === value).count > 1) {
+      items[index].count -= 1;
+      this.setState({ items: items });
+      localStorage.setItem(value, JSON.stringify(items[index]));
+      localStorage.setItem('CartCount', Number(localStorage.getItem('CartCount')) - 1);
+    } else {
+      items.splice(index, 1);
+      localStorage.removeItem(value);
+      this.setState({ items });
+    }
+  }
+
+  carregaProdutos(items, index) {
+    const { title, price, thumbnail, id } = items;
+    return (
+      <div className="containerCarrega" key={index}>
+        {this.clearProduto()}
+        {this.carregaImagemTitulo(title, thumbnail)}
+        <div className="containerBotoes">
+          <div className="somaReduz">
+            <button
+              className="SomeAndRemove"
+              type="button"
+              onClick={() => this.abaixaContador(id)}
+            >
+              <i className="material-icons">remove</i>
+            </button>
+          </div>
+          {items.count}
+          <div className="somaReduz">
+            <button
+              className="SomeAndRemove"
+              type="button"
+              onClick={() => this.aumentaContador(id)}
+            >
+              <i className="material-icons">add</i>
+            </button>
+          </div>
+          {this.valorProduto(price)}
+        </div>
+      </div>
+    );
+  }
+
+  valorProduto(price) {
+    return (
+      <div className="valorProduto">
+        <p>R$ {((price*100)/100).toFixed(2)}</p>
+      </div>
+    );
+  }
+
+  totalProdutos() {
+    const { items } = this.state;
+    return (
+      <div>
+        Valor Total da Compra: R${items.reduce((curr, acc) => { return curr += ((Number(acc.count) * 100) / 100) * ((Number(acc.price) * 100) / 100)}, 0).toFixed(2)}
+      </div>
+    );
+  }
+
+  finalizaCompra() {
+    return (
+      <div>
+        <button>
+          Finalizar Compra
+        </button>
+      </div>
+    );
+  }
+
+  render() {
+    const { load, items } = this.state;
+    if (!load) return ShoppingCart.loadingEmpty();
+    return (
+      <div className="containerGeral">
+        {ShoppingCart.botaoVolta()}
+        {items.map((item, index) => this.carregaProdutos(item, index))}
+        {this.totalProdutos()}
+        {this.finalizaCompra()}
+      </div>
+    );
+  }
+}
