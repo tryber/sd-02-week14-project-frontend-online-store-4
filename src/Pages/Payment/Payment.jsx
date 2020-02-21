ef87dd073ccf31c9fce521fc3faa898cb8670a38
Andrey <andreytsuzuki@gmail.com>
2020-02-21 02:00:31 -0300

    red

--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -91,13 +91,17 @@ class Payment extends Component {
     const campos = this.state.campos;
     const verifica = Object.keys(campos).reduce((acc, key) => {
       if (campos[key].value.length === 0) {
-        this.setState((state) => ({
+        this.setState((state) => {
+          return ({
+          campos: {
           ...state.campos,
           [key]: {
             ...state.campos[key],
-            red: true,
-          } 
-        }));
+            red: true
+          },
+          },
+        });
+        });
         acc = false;
       }
       return acc;
--- src/Pages/Payment/components/Comprador/Comprador.jsx
+++ src/Pages/Payment/components/Comprador/Comprador.jsx
@@ -5,25 +5,25 @@ import './style.css';
 class Comprador extends Component {
   render() {
     const cb = this.props.produtoHandle;
-    const { nome, email } = this.props.campos;
+    const { nome, email, cpf, tel, cep, end, comp, cid, num, est } = this.props.campos;
     return (
       <div className="payment_comprador">
         <p>Informações do comprador</p>
         <div className="container">
           <Input name={'nome'} place={'Nome completo'} cb={cb} red={nome.red} />
           <Input name={'email'} place={'Email'} cb={cb} red={email.red} />
-          <Input name={'cpf'} place={'CPF'} cb={cb} />
-          <Input name={'tel'} place={'Telefone'} cb={cb} />
+          <Input name={'cpf'} place={'CPF'} cb={cb} red={cpf.red} />
+          <Input name={'tel'} place={'Telefone'} cb={cb} red={tel.red} />
         </div>
         <div className="container2">
-          <Input name={'cep'} place={'Cep'} cb={cb} />
-          <Input name={'end'} place={'Endereço'} cb={cb} />
+          <Input name={'cep'} place={'Cep'} cb={cb} red={cep.red} />
+          <Input name={'end'} place={'Endereço'} cb={cb} red={end.red} />
         </div>
         <div className="container3">
-          <Input name={'comp'} place={'Complemento'} cb={cb}  />
-          <Input name={'num'} place={'Número'} cb={cb} />
-          <Input name={'cid'} place={'Cidade'} cb={cb} />
-          <Input name={'est'} place={'Estado'} cb={cb} />
+          <Input name={'comp'} place={'Complemento'} cb={cb} red={comp.red} />
+          <Input name={'num'} place={'Número'} cb={cb} red={num.red} />
+          <Input name={'cid'} place={'Cidade'} cb={cb} red={cid.red} />
+          <Input name={'est'} place={'Estado'} cb={cb} red={est.red} />
         </div>
       </div>
     );
