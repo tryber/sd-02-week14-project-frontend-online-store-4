66cfec58701235721284a51af65a8c142a1f1767
Andrey <andreytsuzuki@gmail.com>
2020-02-20 23:38:52 -0300

    update

--- src/Pages/Payment/components/Comprador/Comprador.jsx
+++ src/Pages/Payment/components/Comprador/Comprador.jsx
@@ -4,25 +4,25 @@ import './style.css';
 
 class Comprador extends Component {
   render() {
-    const { cb } = this.props.produtoHandle;
+    const cb = this.props.produtoHandle;
     return (
       <div className="payment_comprador">
         <p>Informações do comprador</p>
         <div className="container">
-          <Input name={'nome'} place={'Nome completo'} onChange={cb} />
-          <Input name={'email'} place={'Email'} onChange={cb} />
-          <Input name={'cpf'} place={'CPF'} onChange={cb} />
-          <Input name={'tel'} place={'Telefone'} onChange={cb} />
+          <Input name={'nome'} place={'Nome completo'} cb={cb} />
+          <Input name={'email'} place={'Email'} cb={cb} />
+          <Input name={'cpf'} place={'CPF'} cb={cb} />
+          <Input name={'tel'} place={'Telefone'} cb={cb} />
         </div>
         <div className="container2">
-          <Input name={'cep'} place={'Cep'} onChange={cb} />
-          <Input name={'end'} place={'Endereço'} onChange={cb} />
+          <Input name={'cep'} place={'Cep'} cb={cb} />
+          <Input name={'end'} place={'Endereço'} cb={cb} />
         </div>
         <div className="container3">
-          <Input name={'comp'} place={'Complemento'} onChange={cb} />
-          <Input name={'num'} place={'Número'} onChange={cb} />
-          <Input name={'cid'} place={'Cidade'} onChange={cb} />
-          <Input name={'est'} place={'Estado'} onChange={cb} />
+          <Input name={'comp'} place={'Complemento'} cb={cb} />
+          <Input name={'num'} place={'Número'} cb={cb} />
+          <Input name={'cid'} place={'Cidade'} cb={cb} />
+          <Input name={'est'} place={'Estado'} cb={cb} />
         </div>
       </div>
     );
